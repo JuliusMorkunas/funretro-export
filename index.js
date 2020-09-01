419109a8e250fd32b8253f480aca44fbdbcf168d
Julius Morkūnas <julius.morkunas@virtustream.com>
2020-09-01 13:49:45 +0300

    Migrate to Playwright, fix scrapping issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const puppeteer = require('puppeteer');
+const { chromium } = require('playwright');
 
 const [url, file] = process.argv.slice(2);
 
@@ -8,38 +8,42 @@ if (!url) {
   throw 'Please provide a URL as the first argument.';
 }
 
-function run() {
-  return new Promise(async (resolve, reject) => {
-    const browser = await puppeteer.launch();
-    try {
-      const page = await browser.newPage();
-      await page.goto(url);
-      await page.waitForSelector('.message-list');
-      const result = await page.evaluate(() => {
-        const boardTitle = document.querySelector('#board-name').textContent.trim();
-        if (!boardTitle) {
-          throw 'Board title does not exist. Please check if provided URL is correct.'
-        }
-        let parsedText = boardTitle + '\n\n';
-        const columns = document.querySelectorAll('.message-list');
-        columns.forEach((column) => {
-          const columnTitle = column.querySelector('.column-header h2 span[role="button"]').textContent.trim();
-          const messages = [...column.querySelectorAll('.message-main')].map(messageBody => {
-            const messageText = messageBody.querySelector('.message-body .text').textContent.trim();
-            const votes = messageBody.querySelector('.votes .vote-area span.show-vote-count').textContent.trim();
-            return `- ${messageText} (${votes})`;
-          });
-          parsedText = parsedText + columnTitle + '\n' + messages.join('\n') + '\n\n';
-        });
-        return parsedText;
-      });
-      return resolve(result);
-    } catch (e) {
-      return reject(e);
-    } finally {
-      browser.close();
+async function run() {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  await page.goto(url);
+  await page.waitForSelector('.message-list');
+
+  const boardTitle = await page.$eval('#board-name', (node) => node.innerText.trim());
+
+  if (!boardTitle) {
+    throw 'Board title does not exist. Please check if provided URL is correct.'
+  }
+
+  let parsedText = boardTitle + '\n\n';
+
+  const columns = await page.$$('.message-list');
+
+  for (let i = 0; i < columns.length; i++) {
+    const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
+
+    const messages = await columns[i].$$('.message-main');
+    if (messages.length) {
+      parsedText += columnTitle + '\n';
     }
-  });
+    for (let i = 0; i < messages.length; i++) {
+      const messageText = await messages[i].$eval('.message-body .text', (node) => node.innerText.trim());
+      const votes = await messages[i].$eval('.votes .vote-area span.show-vote-count', (node) => node.innerText.trim());
+      parsedText += `- ${messageText} (${votes})` + '\n';
+    }
+
+    if (messages.length) {
+      parsedText += '\n';
+    }
+  }
+
+  return parsedText;
 }
 
 function writeToFile(filePath, data) {
@@ -50,12 +54,12 @@ function writeToFile(filePath, data) {
     } else {
       console.log(`Successfully written to file at: ${resolvedPath}`);
     }
+    process.exit();
   });
 }
 
 function handleError(error) {
   console.error(error);
-  process.exit();
 }
 
 run().then((data) => writeToFile(file, data)).catch(handleError);
